da4c4544c2fdf4152372e292eb20f65fa1beb156
Barry Peddycord III <ishara@isharacomix.org>
2018-11-27 20:37:14 -0500

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -6,4 +6,3 @@ git clone https://github.com/cumulusnetworks/ansible-push-keys
 cd ansible-push-keys; cat /etc/dhcp/dhcpd.hosts | grep 'host .* {' | cut -d " " -f 2 >> hosts
 cd ansible-push-keys; ansible-playbook push-keys.yml --extra-vars 'ansible_ssh_pass=CumulusLinux!' --extra-vars 'ansible_become_pass=CumulusLinux!'
 rm -rf ansible-push-keys
-ansible-playbook setup.yaml
