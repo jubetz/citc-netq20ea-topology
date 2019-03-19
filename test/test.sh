dfa6e1880c029c014f0f2d2744807456559e46a8
jubetz <44002142+jubetz@users.noreply.github.com>
2019-03-19 16:15:41 -0700

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -2,6 +2,10 @@ sudo apt-get install -qy  python-dev libssl-dev  sshpass build-essential libffi-
 sudo pip install setuptools --upgrade
 sudo pip install ansible --upgrade
 rm -rf ansible-push-keys
+sudo mkdir /etc/ansible
+sudo cp hosts /etc/ansible/hosts
+sudo cp ansible.cfg /etc/ansible/ansible.cfg
+sudo chmod -R 777 /etc/ansible
 git clone https://github.com/cumulusnetworks/ansible-push-keys
 cd ansible-push-keys; cat /etc/dhcp/dhcpd.hosts | grep 'host .* {' | cut -d " " -f 2 >> hosts
 cd ansible-push-keys; ansible-playbook push-keys.yml --extra-vars 'ansible_ssh_pass=CumulusLinux!' --extra-vars 'ansible_become_pass=CumulusLinux!'
